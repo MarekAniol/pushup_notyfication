07717aba57cf389b5736b2b6c1392a164a9da509
MarekAniol <marekaniol1@gmail.com>
2022-03-28 12:36:55 +0200

    move of functions to a separate file

--- pushup_Apartment.py
+++ pushup_Apartment.py
@@ -1,19 +1,10 @@
 from libraries import *
 from os import getcwd
+from functions import *
 
 HERE = getcwd()
 TOKEN = config("PUSH_UP_TOKEN")
 pb = Pushbullet(TOKEN)
-
-
-def get_apartments(www):
-    """Take url as argument and return iterable bs4 object.
-    - html parts with tiles and offer links
-    """
-    page = get(www)
-    content_html = Bfs(page.text, "html.parser")
-    apartments = content_html.find_all(class_="offer-wrapper")
-    return apartments
 
 
 def write_json(data):
@@ -27,21 +18,6 @@ def load_apartments_id():
     with open(HERE + r"/found_products.json", "r") as products_file:
         d = json.load(products_file)
         return d
-
-
-def check_id(apart, apart_price, ids):
-    """Check if apart apart_id is in loaded data from json file. If not add apart_price
-    to found_products.json by call write_json()
-    """
-    price_digit = int(apart_price.replace(" zł", "").replace(" ", ""))
-    apart_id = apart.select_one("table")["data-id"]
-    if apart_id not in ids:
-        ids[apart_id] = price_digit
-        print(ids)
-        write_json(ids)
-        return True
-    else:
-        return False
 
 
 def main():
